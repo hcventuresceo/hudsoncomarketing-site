31480bd84f0cdc638b8b044c51e9c50c304b9052
hcventuresceo <admin@hudsoncoventures.com>
2026-02-11 22:33:34 -0500

    Delete src directory

--- src/components/RavenLogo.tsx
+++
@@ -1,7 +0,0 @@
-export function RavenLogo() {
-  return (
-    <div className="flex items-center gap-3">
-      <span className="text-lg text-[#F2D27C]">Hudson & Co Marketing</span>
-    </div>
-  );
-}
